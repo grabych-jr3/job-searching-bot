9d08f50145cccf8501e9d83982254f5088abe9ad
ogidazepam <v.grabovskyi10@gmail.com>
2026-08-14 12:44:24 +0200

    initialized job-api-service

---
+++ job-api-service/src/main/java/com/ogidazepam/job_api_service/JobApiServiceApplication.java
@@ -0,0 +1,13 @@
+package com.ogidazepam.job_api_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobApiServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobApiServiceApplication.class, args);
+	}
+
+}
